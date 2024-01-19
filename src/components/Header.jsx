45b2dfa40e2b08f61f6172dffeb951375abf94de
jaengustavof <gustavojv@clickandgo.com>
2024-01-19 18:16:40 +0100

    nuevo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <h1 className="font-black text-3xl text-center md:w-2/3 mx-auto mb-20 mt-5">Seguimiento Pacientes {""}
+        <h1 className="font-black text-3xl text-center md:w-2/3 mx-auto mb-20 mt-5">Seguimiento Pacientes {""} con Github
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
 
